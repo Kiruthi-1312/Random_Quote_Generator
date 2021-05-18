31a0c4f708fb966e8946f2ce0902d9ef2e0e8bdb
Kiruthika <kiruthikaoff13@gmail.com>
2021-05-18 18:58:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    quote: "",
+  };
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  fetchQuote = () => {
+    let randomNo = Math.floor(Math.random() * 1643);
+    axios
+      .get("https://type.fit/api/quotes ")
+      .then((response) => {
+        const quote = response.data[randomNo].text;
+        this.setState({ quote });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { quote } = this.state;
+    return (
+      <div className="app">
+        <h1 className="title">RANDOM QUOTES</h1>
+        <div className="card">
+          <h1 className="heading">{quote}</h1>
+        </div>
+        <button className="button" onClick={this.fetchQuote}>
+          Generate a Quote
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
